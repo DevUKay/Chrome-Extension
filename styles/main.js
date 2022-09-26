6c6336f9efc5cccea97e1cdf68622a5a47847649
Dev Ukay <kingsleykrest@gmail.com>
2022-09-27 03:55:36 +1400

    fix

--- styles/main.js
+++ styles/main.js
@@ -1,22 +1,35 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    // console.log(myLeads)
+    renderLeads()
 })
 
-for (i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+
+
+function renderLeads() {
+    
+    let listItems = " "
+    for (i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        console.log(listItems)
+    }
+    
+    ulEl.innerHTML = listItems
+
 }
+
 
 //////////////////////// Below here!| praticing rendering HTML code through JS
 
-let buyBtn = document.getElementById('container')
+// let buyBtn = document.getElementById('container')
 
-buyBtn.innerHTML += "<button onclick='buy()'> Buy! </button>"
-function buy() {
-    buyBtn.innerHTML += "<p> Thank you for Buying! </p>"
-}
+// buyBtn.innerHTML += "<button onclick='buy()'> Buy! </button>"
+// function buy() {
+//     buyBtn.innerHTML += "<p> Thank you for Buying! </p>"
+// }
